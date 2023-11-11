7975f5eca66c6964d1f6e93ff3450e39fc14bc6a
svalka <angeddonia@gmail.com>
2023-11-11 23:55:32 +0200

    Добавьте файлы проекта.

---
+++ 53/53.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double x);
+int main()
+{
+	double yp, yk, z;
+	int n;
+	cout << "yp = "; cin >> yp;
+	cout << "yk = "; cin >> yk;
+	cout << "n = "; cin >> n;
+	double dy = (yk - yp) / n;
+	double y = yp;
+	while (y <= yk)
+	{
+		z = f(1 + y * y) + (f(f(1) + f(y * y) * (f(y * y))) * f(f(1) + f(y * y) * f(y * y)));
+		cout << y << " " << z << endl;
+		y += dy;
+	}
+	return 0;
+}
+double f(const double x)
+{
+	if (abs(x) >= 1)
+		return cos(x / 2) / (1 + sin(x) * sin(x));
+	else
+	{
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = a;
+		do
+		{
+			i++;
+			double R = x * (2 * i - 1) * 2 * i;
+			a *= R;
+			S += a;
+		} while (i < 6);
+		return S;
+	}
+}
---
+++ UnitTest53/UnitTest53.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../53/53.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = f(1);
+			Assert::AreEqual(t, 0.513785, 0.000001);
+		}
+	};
+}
